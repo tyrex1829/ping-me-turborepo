f9d3371ebd25eece7c81a7fc9fe91e6a78e81641
Saksham Khare <sakshamkhare19@gmail.com>
2025-07-28 23:34:30 +0530

    Feat: prisma db at packages and import at http-server

--- apps/http-server/src/controller/chat.controller.ts
+++ apps/http-server/src/controller/chat.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { prisma } from "@repo/db/prisma";
 
 export const chat = async (req: Request, res: Response): Promise<void> => {
+  const user = await prisma.user.findFirst();
   return;
 };
