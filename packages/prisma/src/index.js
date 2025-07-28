bce973250907a75d7117c010de36afd2fa294e42
tyrex1829 <sakshamkhare19@gmail.com>
2025-07-29 03:36:05 +0530

    Feat: fix for linux

--- packages/prisma/src/index.js
+++ packages/prisma/src/index.js
@@ -11,3 +11,5 @@ const globalForPrisma = globalThis;
 exports.prisma = globalForPrisma.prisma || new client_1.PrismaClient();
 if (process.env.NODE_ENV !== "production")
     globalForPrisma.prisma = exports.prisma;
+//
+// <
--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -9,3 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+//
+
+// <
